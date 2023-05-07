49dc0d92d1afe7d71553ef09f69763555cd203fd
kingsley <oparaudochukwu277@gmail.com>
2023-05-07 10:19:01 +0100

    getting the host

--- backend/school_portal/settings.py
+++ backend/school_portal/settings.py
@@ -28,9 +28,9 @@ DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 
 
-ALLOWED_HOSTS = ['http://*', 'https://*']
-# if not DEBUG:
-#     ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['http://*', 'https://*', 'https://godocs.up.railway.app/']
+if not DEBUG:
+    ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -285,4 +285,4 @@ if POSTGRES_READY:
         }
     }
 
-CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS.copy()
+# CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS.copy()
