4f7e0611c16a7d7901ae42510668a505edf911c6
kingsley <oparaudochukwu277@gmail.com>
2023-09-13 21:34:31 +0100

    changed allowed_host

--- backend/school_portal/settings.py
+++ backend/school_portal/settings.py
@@ -29,8 +29,8 @@ DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 
 ALLOWED_HOSTS = []
-# if not DEBUG:
-#     ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+if not DEBUG:
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
