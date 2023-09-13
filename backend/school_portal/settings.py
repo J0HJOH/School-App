b2a9251de102f6f4d2c2fd4d25ff68482f984771
kingsley <oparaudochukwu277@gmail.com>
2023-09-13 22:08:46 +0100

    make changes to settings.py file

--- backend/school_portal/settings.py
+++ backend/school_portal/settings.py
@@ -28,14 +28,13 @@ DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 
 
-ALLOWED_HOSTS = []
-if not DEBUG:
-    ALLOWED_HOSTS = ['*', os.environ.get('ALLOWED_HOSTS'), 'godocs.onrender.com']
+ALLOWED_HOSTS = ['*']
+
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 print(RENDER_EXTERNAL_HOSTNAME, 'rendder host is going')
 # Application definition
 
